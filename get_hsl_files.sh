68dd1c74883f200338fdaf3bb8fa3df82897451c
Merlin Sauer <merlin.sauer@aalto.fi>
2026-08-03 10:40:55 +0300

    Removed otp-config.json from get-hsl_files.sh, since it overwrites own config

--- get_hsl_files.sh
+++ get_hsl_files.sh
@@ -1,6 +1,6 @@
 BASE="https://api.digitransit.fi/routing-data/v3/hsl"
 KEY="63aa748c4b994123b62784d466fb3ae5"
 
-for FILE in HSL-gtfs.zip hsl.pbf HSLlautta-gtfs.zip build-config.json otp-config.json router-config.json; do
+for FILE in HSL-gtfs.zip hsl.pbf HSLlautta-gtfs.zip build-config.json router-config.json; do
   curl -H "digitransit-subscription-key: $KEY" "$BASE/$FILE" -o "./hsl/$FILE"
 done
